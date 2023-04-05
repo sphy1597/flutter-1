5d7d077f2452345797f34aa8f0403f970d520091
favorcat <favorcat.me@gmail.com>
2023-04-05 21:14:21 +0900

    메인화면 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,287 @@
+import 'package:app/qr.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'button',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyButton(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyButton extends StatelessWidget {
+  const MyButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text('약리미',
+          style: new TextStyle(
+            fontSize: 30.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body:
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 1,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+            child: Align(
+              alignment: const AlignmentDirectional(0, 0),
+              child:Column(
+                children: [
+                  Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Expanded( // QR코드 검색
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            SizedBox(
+                              height: 130,
+                              width: 130,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  print('QR코드 검색 button');
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => const QrApp()),
+                                  );
+                                },
+                                child: Text('QR코드 검색',
+                                  style: new TextStyle(
+                                    fontSize: 26.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    maximumSize: const Size(130,130),
+                                    // padding: const EdgeInsets.all(10),
+                                    elevation: 0.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded( // 이미지 검색
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            SizedBox(
+                              height: 130,
+                              width: 130,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  print('이미지 검색');
+                                },
+                                child: Text('이미지 검색',
+                                  style: new TextStyle(
+                                    fontSize: 26.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    maximumSize: const Size(130,130),
+                                    // padding: const EdgeInsets.all(10),
+                                    elevation: 0.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Expanded( // 약 검색
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            SizedBox(
+                              height: 130,
+                              width: 130,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  print('약 검색 button');
+                                },
+                                child: Text('약 검색',
+                                  style: new TextStyle(
+                                    fontSize: 26.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    maximumSize: const Size(130,130),
+                                    // padding: const EdgeInsets.all(10),
+                                    elevation: 0.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded( // 글자 인식
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            SizedBox(
+                              height: 130,
+                              width: 130,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  print('글자 인식 button');
+                                },
+                                child: Text('글자 인식',
+                                  style: new TextStyle(
+                                  fontSize: 26.0,
+                                  color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    maximumSize: const Size(130,130),
+                                    // padding: const EdgeInsets.all(10),
+                                    elevation: 0.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Expanded( // 복용 알림
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            SizedBox(
+                              height: 130,
+                              width: 130,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  print('복용 알림 button');
+                                },
+                                child: Text('복용 알림',
+                                  style: new TextStyle(
+                                    fontSize: 26.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    maximumSize: const Size(130,130),
+                                    // padding: const EdgeInsets.all(10),
+                                    elevation: 0.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded( // 검색 기록
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child:
+                            SizedBox(
+                              height: 130,
+                              width: 130,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  print('검색 기록 button');
+                                },
+                                child: Text('검색 기록',
+                                  style: new TextStyle(
+                                    fontSize: 26.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    maximumSize: const Size(130,130),
+                                    // padding: const EdgeInsets.all(10),
+                                    elevation: 0.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(10),
+                      child:
+                      SizedBox(
+                        height: 60,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('환경설정 button');
+                          },
+                          child: Text('환경설정',
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              // padding: const EdgeInsets.all(10),
+                              elevation: 0.0),
+                        ),
+                      ),
+                    ),
+                  // Container(
+                  //   color:Colors.red,
+                  //   width: 100,
+                  //   height: 100,
+                  //   padding: const EdgeInsets.all(8.0),
+                  // )
+                ],
+              ),
+            )
+          )
     );
   }
 }
