54f66448420541fa3ac9cdddcc05b64149495192
sphy1597 <sphy1597@naver.com>
2023-04-14 16:09:18 +0900

    QR info UI
    QR정보 넣을 UI 완성

--- lib/qr.dart
+++ lib/qr.dart
@@ -46,48 +46,124 @@ class MyButton extends StatelessWidget {
               alignment: const AlignmentDirectional(0, 0),
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: SizedBox(
-                      height: 60,
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print('카메라 사용하기 button');
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const QRcodeWidget()),
-                          ).then((value) {
-                            String receive = value;
-                            print('전달받은 값 : $receive');
-                          });
-                        },
-                        child: Text(
-                          '카메라 사용하기',
-                          style: new TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('카메라 사용하기 button');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const QRcodeWidget()),
+                            ).then((value) {
+                              String receive = value;
+                              print('전달받은 값 : $receive');
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                              primary:
+                                  Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              // padding: const EdgeInsets.all(10),
+                              elevation: 0.0),
+                          child: Text(
+                            '카메라 사용하기',
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            // padding: const EdgeInsets.all(10),
-                            elevation: 0.0),
                       ),
                     ),
                   ),
-
-                  // Container(
-                  //   color:Colors.red,
-                  //   width: 100,
-                  //   height: 100,
-                  //   padding: const EdgeInsets.all(8.0),
-                  // )
+                  Expanded(
+                      //약 이름
+                      flex: 1,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(255, 242, 227, 219)),
+                          child: Center(
+                            child: Text(
+                              '약 이름',
+                              style: TextStyle(
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                    // 복용법
+                    flex: 1,
+                    child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color.fromRGBO(243, 232, 255, 1)),
+                            child: Center(
+                              child: Text(
+                                '복용법',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ))),
+                  ),
+                  Expanded(
+                      //효능
+                      flex: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromRGBO(220, 250, 210, 1)),
+                          child: Center(
+                            child: Text(
+                              '효능',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                      //주의사항
+                      flex: 3,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromRGBO(230, 230, 230, 1)),
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              '주의사항',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )),
                 ],
               ),
             )));
