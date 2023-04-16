35bae621fb6a72b736f715d145b17de72442feb2
favorcat <favorcat.me@gmail.com>
2023-04-16 11:58:27 +0900

    edit: 환경설정 - 앱 환경설정 부분 삭제

--- lib/settings/settings_main.dart
+++ lib/settings/settings_main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:app/main.dart';
 import 'package:app/settings/user_info.dart';
+import 'package:app/settings/allergy_info.dart';
+// import 'package:app/settings/app_setting.dart';
 
 void main() => runApp(const SettingsMain());
 
@@ -101,6 +103,12 @@ class SettingsMain extends StatelessWidget {
                       child: ElevatedButton(
                         onPressed: () {
                           print('질병 및 알러지 정보 btn 눌림');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const AllergyInfo(title: '',)
+                            ),
+                          );
                         },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.orangeAccent[100], // 텍스트 버튼과 다르게 배경색 변경
@@ -121,36 +129,42 @@ class SettingsMain extends StatelessWidget {
                     ),
                   ),
                 ),
-                Expanded(
-                  // 앱 환경설정
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print('앱 환경설정 btn 눌림');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          // padding: const EdgeInsets.all(10),
-                          elevation: 0.0
-                        ),
-                        child: const Text(
-                          '앱 환경설정',
-                          style: TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                // Expanded(
+                //   // 앱 환경설정
+                //   flex: 1,
+                //   child: Padding(
+                //     padding: const EdgeInsets.all(10),
+                //     child: SizedBox(
+                //       width: double.infinity,
+                //       child: ElevatedButton(
+                //         onPressed: () {
+                //           print('앱 환경설정 btn 눌림');
+                //           Navigator.push(
+                //             context,
+                //             MaterialPageRoute(
+                //                 builder: (context) => const MyStatefulWidget()
+                //             ),
+                //           );
+                //         },
+                //         style: ElevatedButton.styleFrom(
+                //           backgroundColor: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
+                //           shape: RoundedRectangleBorder(
+                //             borderRadius: BorderRadius.circular(10.0),
+                //           ),
+                //           // padding: const EdgeInsets.all(10),
+                //           elevation: 0.0
+                //         ),
+                //         child: const Text(
+                //           '앱 환경설정',
+                //           style: TextStyle(
+                //             fontSize: 30.0,
+                //             color: Colors.black,
+                //           ),
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
                 Expanded(
                   // 음성 도움말
                   flex: 1,
@@ -163,7 +177,7 @@ class SettingsMain extends StatelessWidget {
                           print('음성 도움말 btn 눌림');
                         },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.orangeAccent[100], // 텍스트 버튼과 다르게 배경색 변경
+                          backgroundColor: Colors.orangeAccent, // 텍스트 버튼과 다르게 배경색 변경
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10.0),
                           ),
